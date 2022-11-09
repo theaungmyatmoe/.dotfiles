2100643f643021f0ec3e5a3e5bf2ad4719a136d5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-11-10 04:11:37 +0630

    feat: change telegrap file searcher to dropdown style

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,28 @@
 local telescope_status, telescope = pcall(require, "telescope")
-if not telescope_status then return end
+if not telescope_status then
+	return
+end
 
 local actions_status, actions = pcall(require, "telescope.actions")
-if not actions_status then return end
+if not actions_status then
+	return
+end
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-p>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-n>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-      },
-    },
-  },
+	defaults = {
+		mappings = {
+			i = {
+				["<C-p>"] = actions.move_selection_previous, -- move to prev result
+				["<C-n>"] = actions.move_selection_next, -- move to next result
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			theme = "dropdown",
+		},
+	},
 })
 
 telescope.load_extension("fzf")
